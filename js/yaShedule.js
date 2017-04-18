3035249ebf8eca8ebd922144fa0d1da379e2c1c5
UDemis <support@demis.ru>
2017-04-18 18:17:40 +0300

    Added doc help() and some methods for lections. Get, Set, Edit

--- js/yaShedule.js
+++ js/yaShedule.js
@@ -1,7 +1,5 @@
 function Lection(){ 
-	this.name;
-	//this.school = [];
-	this.lector = [];
+
 }
 Lection.prototype.id = 0;
 Lection.prototype.lections = [];
@@ -9,18 +7,35 @@ Lection.prototype._unique = function( array ){
 	var obj = {};
 	for (var i = 0; i < array.length; i++) {
 	   var str = array[i];
-	   obj[str] = true; // запомнить строку в виде свойства объекта
+	   obj[str] = true;
 	}
 	return Object.keys(obj);
 }
+Lection.prototype.help = function(){
+	return 'Начать: var lection = new Lection(); \n' +
+	'Задать лекции и свойства: this.setLection( { property_name : property(string) } )\n' +
+	'Редактировать лекции: this.editLection( id_лекции, { property_name : property(string) } )\n' +
+	'Удалить лекцию: this.deleteLection( id_лекции ) \n' +
+	'Добавить школу к лекции: this.addSchoolToLection( school1[, school2, school3...])\n' +
+	'Добавить лектора: this.addLectorToLection( lector1[, lector2, lector3...])\n';
+}
 Lection.prototype.setLection = function(obj){
-	this.name = obj.name;
+	if ( !obj ){
+		throw new Error('Нельзя создать лекцию без названия.');
+	}
+	if ( typeof(obj) != "object" ){
+		throw new Error('Неправильно заданы параметры. \n'+
+					    'Обратитесь к документации библиотеки help()');
+	}
 	this.lections.push({
 	  name : obj.name,
 	  id : ++this.id,
-	  school : []
+	  school : [],
+	  lector : [],
+	  time : []
 	});
 }
+Lection.prototype._editableProperties = [ 'name', 'time' ];
 Lection.prototype.getLection = function(){
 	var arg = arguments;
 	if (arg.length == 0) {
@@ -34,30 +49,65 @@ Lection.prototype.getLection = function(){
 	      }
 		});
 	}
+	if ( filteredLections.length == 0 ){
+		throw new Error('Такой лекции нет или она была удалена! \n' +
+					   	' Посмотреть текущие лекции: getLections() ');
+	}
 	return filteredLections;
 }
-Lection.prototype.deleteLection = function(){
-// 	var arg = arguments;
-// 	for (var i = 0; i < arg.length; i++) {
-// 	    this.lections.map(function(lection) {
-// 	      if (lection.id == arg[i]){
-// 	      	console.log(lection);
-// 	      	lection = null;
-// 	      }
-// 		});
-// 	}
-// 	console.log(this.lections);
-// arr.forEach(function(item, i, arr) {
-//   alert( i + ": " + item + " (массив:" + arr + ")" );
-});
+Lection.prototype.editLection = function( num, properties ){
+		/*
+		*
+		* Лекцию можно отредактировать только
+		* по очереди, множественное редактирование
+		* не нужно. Для лекций и школ отдельные функции,
+		* так как нужна возможность не только менять, но
+		* и удалять
+		*
+		*/
+		var lection = this.getLection(num)[0];
+		for (var i = 0; i < this._editableProperties.length; i++) {
+				if ( properties[ this._editableProperties[i] ] ){
+						lection[ this._editableProperties[i] ] = properties[ this._editableProperties[i] ];
+				}
+		}
+}
+Lection.prototype.deleteLection = function(num){
+		if (num < 1){
+			throw new Error("Введите положительный ненулевой id элемента")
+		}
+		this.getLection(num);
+		this.lections.splice(num - 1, 1);
 }
 Lection.prototype.addSchoolToLection = function( arrLections, school ){
-		var school = this._unique( school.split(', ') );
+		var school = school.split(', ');
 		for (var i = 0; i < arrLections.length; i++) {
 			for (var j = 0; j < school.length; j++) {
 					arrLections[i].school.push(school[j]);
 			}
+			arrLections[i].school = this._unique(arrLections[i].school);
 		}
+		return 'Добавлены ' + this._unique(school);
+}
+Lection.prototype.editSchoolToLection = function(){
+		var school = school.split(', ');
+		for (var i = 0; i < arrLections.length; i++) {
+			for (var j = 0; j < school.length; j++) {
+					arrLections[i].school.push(school[j]);
+			}
+			arrLections[i].school = this._unique(arrLections[i].school);
+		}
+		return 'Добавлены ' + this._unique(school);
+}
+Lection.prototype.addLectorToLection = function( arrLections, lector ){
+		var lector = this._unique( lector.split(', ') );
+		for (var i = 0; i < arrLections.length; i++) {
+			for (var j = 0; j < lector.length; j++) {
+					arrLections[i].lector.push(lector[j]);
+			}
+			arrLections[i].lector = this._unique(arrLections[i].lector);
+		}
+		return 'Добавлены ' + this._unique(lector);
 }
 function School(){
 	this._name;
@@ -88,7 +138,7 @@ schools.setSchool({
 schools.setSchool({
 	title: 'shri',
 	count: 40
-});
+}); // добавит две школы
 var lection = new Lection();
 lection.setLection({
 	name: "ООП",
@@ -101,7 +151,11 @@ lection.setLection({
 });
 lection.setLection({
 	name: "Margin"
-});
-var lol = lection.getLection(1);
+}); // добавит 4 лекции
 lection.addSchoolToLection( lection.getLection(1,3,4), 'shmd, shri, shri, lolz, lolz, shmd'); //добавит только shmd, shri, lolz
-lection.deleteLection(2);
+lection.deleteLection(1); // удалит лекцию с id = 1
+lection.editLection( 2, {
+	name: "Мобильная лекция для начинающих"
+}); // отредактирует название у лекции с id = 2
+lection.addSchoolToLection( lection.getLection(1,3,4), 'shmd, shri, shri, lolz, lolz, shmd'); // не добавит дубли
+lection.addLectorToLection( lection.getLection(2,4), 'Алексей Тяпкин'); //добавит Алексея Тяпкина
