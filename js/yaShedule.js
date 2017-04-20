dc805abe14fd1c491a83def17070a8bb3870db3b
yepstepz <prettyoptimist@gmail.com>
2017-04-21 01:22:22 +0300

    tests + FUNCTION fixes

--- js/yaShedule.js
+++ js/yaShedule.js
@@ -46,9 +46,10 @@ Core.prototype.getEntity = function(){
 	}
 	var filteredEntities = [];
 	for (var i = 0; i < arg.length; i++) {
-	    this.entities.filter(function(lection) {
-	      if (lection.id == arg[i]){
-	      	filteredEntities.push(lection);
+	    this.entities.filter(function(entity) {
+	      if (entity.id == arg[i]){
+
+	      	filteredEntities.push(entity);
 	      }
 		});
 	}
@@ -58,140 +59,124 @@ Core.prototype.getEntity = function(){
 	}
 	return filteredEntities;
 }
-Core.prototype.editEntity = function( num, properties ){
+Core.prototype.editName = function( num, name ){
 		/*
 		*
 		* Сущность можно отредактировать только
 		* по очереди, множественное редактирование
-		* не нужно. Для лекций и школ отдельные функции,
-		* так как нужна возможность не только менять, но
-		* и удалять
+		* не нужно. 
 		*
 		*/
-		var lection = this.getEntity(num)[0];
-		for (var i = 0; i < this._editableProperties.length; i++) {
-				if ( properties[ this._editableProperties[i] ] ){
-						lection[ this._editableProperties[i] ] = properties[ this._editableProperties[i] ];
-				}
+		if ( typeof(name) != 'string' ){
+
+			throw new Error('Имя нужно указать в виде строки!');
 		}
-		return lection;
+		if ( arguments < 2 || !name || name == '' ) {
+			throw new Error('Вы не ввели имя!');
+		}
+		var entity = this.getEntity(num)[0];
+			entity[ 'name' ] = name;
+			return entity[ 'name' ];
 }
 Core.prototype.deleteEntity = function(num){
+		var del = {};
+		var count = 0;
+		this.getEntity( num );
 		if (num < 1){
 			throw new Error("Введите положительный ненулевой id элемента")
 		}
 		var arg = arguments;
 		var index;
-		var deleted = [];
-		this.getEntity(num);
 		for (var i = 0; i < arg.length; i++) {
-		    this.entities.filter(function(lection, k) {
-		      if (Core.id == arg[i]){
+		    this.entities.filter(function(entity, k) {
+		      if (entity.id == arg[i]){
 		      	index = k;
 		      }
 			});
-	      	deleted.push(this.entities.splice(index, 1)[0]);
+	      	del[count++] = this.entities.splice(index, 1)[0];
 		}
-		return deleted;
+		return del;
+		
 }
 Core.prototype.addObjectToEntity = function( arrEntities, type, object ){
 		var object = object.split(', ');
-		console.log(object);
-		//var school = school.split(', ');
+		for (var i = 0; i < arrEntities.length; i++) {
+			arrEntities[i] = this.getEntity( arrEntities[i] )[0];
+		}
 		for (var i = 0; i < arrEntities.length; i++) {
 			for (var j = 0; j < object.length; j++) {
-					arrEntities[i][type].push(object[j]);
+					this._unique(arrEntities[i][type].push(object[j]));
 			}
 			arrEntities[i][type] = this._unique(arrEntities[i][type]);
+
 		}
-		return 'Добавлены ' + this._unique(object);
+		return 'Добавлены ' + this._unique( object );
 }
-// Core.prototype.addLectorToEntity = function( arrEntities, lector ){
-// 		var lector = lector.split(', ');
-// 		for (var i = 0; i < arrEntities.length; i++) {
-// 			for (var j = 0; j < lector.length; j++) {
-// 					arrEntities[i].lector.push(lector[j]);
-// 			}
-// 			arrEntities[i].lector = this._unique(arrEntities[i].lector);
-// 		}
-// 		return 'Добавлены ' + this._unique(lector);
-// }
+Core.prototype.changeEntityObject = function(arrEntities, type, oldNames, newNames){
+		var oldNames = oldNames.split(', ');
+		var newNames = newNames.split(', ');
+		if (oldNames.length != newNames.length){
+			throw new Error('Вы пытаетесь изменить массивы разной длины');
+		}
+		for (var i = 0; i < arrEntities.length; i++) {
+			arrEntities[i] = this.getEntity( arrEntities[i] )[0];
+			for (var j = 0; j < newNames.length; j++) {
+				var index = this._find( arrEntities[i][type], oldNames[j]);
+				entity[type][index] = args[4][i];
+			}
+		}
+		this._unique(entity[type]);
+		return "Элементы изменены";	
+}
 Core.prototype.editArrayEntity = function(){
 		var args = arguments;
-		var lection;
+		var entity;
 	    this.entities.filter(function(item) {
 	      if (item.id == args[0]){
-	      	lection = item;
+	      	entity = item;
 	      }
 		});
 		var type = args[1];
 		var del = [];
 		if (args.length == 3 && args[2] == 0 ) {
-			lection[type] = [];
-			return lection[type];
+			entity[type] = [];
+			return "Элементы удалены";
 		}
 		if ( args.length == 4 && args[2] == 0 ){
 			for (var i = 0; i < args[3].length; i++) {
-				var index = this._find( lection[type], args[3][i]);
+				var index = this._find( entity[type], args[3][i]);
 				if ( index != -1 ){
-					lection[type].splice( index, 1);
+					entity[type].splice( index, 1);
 				}
 			}
 			return "Элементы удалены";
-		}
-		if (args.length == 4 && args[2] != 0 ){
-			for (var i = 0; i < args[3].length; i++) {
-				lection[type].push( args[3][i] );
-			}
-			this._unique(lection[type]);
-			return "Элементы добавлены";			
 		}
 		if (args.length == 5 && args[2] != 0 ){
 			if (args[3].length != args[4].length){
 				throw new Error('Вы пытаетесь изменить массивы разной длины');
 			}
 			for (var i = 0; i < args[4].length; i++) {
-				var index = this._find( lection[type], args[3][i]);
-				lection[type][index] = args[4][i];
+				var index = this._find( entity[type], args[3][i]);
+				entity[type][index] = args[4][i];
 			}
-			this._unique(lection[type]);
+			this._unique(entity[type]);
 			return "Элементы изменены";			
 		}
+}
+Core.prototype.addLectorToEntity = function( arrEntities, lector ){
+		var lector = this._unique( lector.split(', ') );
+		for (var i = 0; i < arrEntities.length; i++) {
+			for (var j = 0; j < lector.length; j++) {
+					arrEntities[i].lector.push(lector[j]);
+			}
+			arrEntities[i].lector = this._unique(arrEntities[i].lector);
+		}
+		return 'Добавлены ' + this._unique(lector);
 }
 function Lection(){ 
 	Core.apply(this, arguments);
 }
 Lection.prototype = Object.create(Core.prototype);
-function School(){
-	this._name;
-	this._count;
-	this.entities = [];
-}
-School.prototype.schools = [];
-School.prototype.setSchool = function(){
-    for (var i = 0; i < arguments.length; i++) {
-      this.schools.push(arguments[i]);
-      this.name = arguments[i].title;
-      this.count = arguments[i].title;
-    }
-}
-School.prototype.getSchool = function(){
-	return this.schools.slice();
-}
-function Cabinet(name, capacity){
-	this._name = name;
-	this._capacity = capacity;
-}
-//Проверка
-var schools = new School();
-schools.setSchool({
-	title: 'shmd',
-	count: 30
-});
-schools.setSchool({
-	title: 'shri',
-	count: 40
-}); // добавит две школы
 var lection = new Lection();
 lection.setEntity({
 	name: "ООП",
@@ -205,17 +190,3 @@ lection.setEntity({
 lection.setEntity({
 	name: "Margin"
 }); // добавит 4 лекции
-lection.addObjectToEntity( lection.getEntity(1,3,4), 'school', 'shmd, shri, shri, lolz, lolz, shmd'); //добавит только shmd, shri, lolz
-lection.deleteEntity(1); // удалит лекцию с id = 2
-lection.editEntity( 2, {
-	name: "Мобильная лекция для начинающих"
-}); // отредактирует название у лекции с id = 2
-lection.addObjectToEntity( lection.getEntity(1,3,4), 'school', 'shmd, shri, shri, lolz, lolz, shmd'); // не добавит дубли
-lection.addObjectToEntity( lection.getEntity(2,4), 'lector', 'Алексей Тяпкин'); //добавит Алексея Тяпкина
-//lection.editArrayEntity( 3, 'school', 0 ); // удалит все
-//lection.editArrayEntity( 3, 'school', 0, ['shmd2', 'shmd']); // удалит shmd
-//lection.editArrayEntity( 3, 'school', 0, ['shri', 'shmd']); // удалит два класса 'shri', 'shmd'
-//lection.editArrayEntity( 3, 'school', 1, ['shmd2', 'shmr2'] ); // добавит два класса 'shmd2', 'shmr2'
-//lection.editArrayEntity( 3, 'school', 1, ['shri, lolz'], ['shmr', 'lolz2'] ); // ошибка, массивы разной длины.
-//lection.editArrayEntity( 3, 'school', 1, ['shri', 'lolz'], ['shmr', 'lolz2'] ); // вернет "shmd", "shmr", "lolz2"
-//lection.editArrayEntity( 3, 'lector', 1, ['Арсений', 'Иван']); // Добавит двух Лекторов
--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,56 @@
 describe("Lection", function() {
   describe("constructor", function() {
+  	describe("Удаление элемента", function() {
 	  it("удалит лекцию c указанным id", function() {
 	  	var lection = new Lection();
-	    assert.equal(lection.deleteEntity(1)[0].id, 1);
+	    assert.equal(lection.deleteEntity(1)[0].id, 1 );
 	  });
 	  it("выдает ошибку при попытке удалить повторно", function() {
 	  	var lection = new Lection();
 	    assert.throws(function () { lection.deleteEntity(1)[0].id }, Error);
 	  });
-	  it("добавит классы", function() {
+  	});
+	describe("Добавление элемента", function() {
+	  it("добавит школы", function() {
 	  	var lection = new Lection();
-	   	assert.equal(lection.addObjectToEntity( lection.getEntity(1,3,4), 'school', 'shmd, shri, shri, lolz, lolz, shmd'), 'Добавлены shmd,shri,lolz');
+	   	assert.equal(lection.addObjectToEntity( [2, 3], 'school', 'shmd, shri, shri, lolz, lolz, shmd'), 'Добавлены shmd,shri,lolz');
 	  });
-	  it("добавит классы", function() {
+	  it("добавит лекторов", function() {
 	  	var lection = new Lection();
-	   	assert.equal(lection.addObjectToEntity( lection.getEntity(2,4), 'lector', 'Алексей Тяпкин'), 'Добавлены Алексей Тяпкин');
+	   	assert.equal(lection.addObjectToEntity( [2, 3], 'lector', 'Алексей Афанасьев, Виктор Сухов'), 'Добавлены Алексей Афанасьев,Виктор Сухов');
 	  });
-	  it("изменит название", function() {
+	  it("не добавит дубли", function() {
 	  	var lection = new Lection();
-	   	assert.equal(lection.editEntity( 2, {
-	   				 name: "Мобильная лекция для начинающих"
-	   	}).name, 'Мобильная лекция для начинающих');
+	   	assert.equal(lection.addObjectToEntity( [2, 3], 'lector', 'Роман Дмитриев, Роман Дмитриев, Алексей Афанасьев'), 'Добавлены Роман Дмитриев,Алексей Афанасьев');
+	  });
+	});
+	describe("Изменение элемента", function() {
+	  it("изменить школы", function() {
+	  	var lection = new Lection();
+	   	assert.equal(lection.changeEntityObject( [2, 3], 'school', 'shmd, shri', 'shmd2, shri2'), 'Добавлены shmd,shri,lolz');
+	  });		
+	});	
+	  describe("Изменение название", function() {
+		  it("изменит название", function() {
+		  	var lection = new Lection();
+		   	assert.equal( lection.editName( 2, "Мобильная лекция для начинающих"), 'Мобильная лекция для начинающих' );
+		  });
+		  it("Возвратит ошибку 'Вы не ввели имя'", function() {
+		  	var lection = new Lection();
+		   	 assert.throws(function () { lection.editName( 2 ) }, Error );
+		  });
+		  it("Возвратит ошибку 'Вы не ввели имя'", function() {
+		  	var lection = new Lection();
+		   	 assert.throws(function () { lection.editName( 2, k ) }, Error );
+		  });
+		  it("Возвратит ошибку 'Вы не ввели имя'", function() {
+		  	var lection = new Lection();
+		   	 assert.throws(function () { lection.editName( 2, '' ) }, Error );
+		  });
+		  it("Возвратит ошибку 'Имя нужно указать в виде строки'", function() {
+		  	var lection = new Lection();
+		   	 assert.throws(function () { lection.editName( 2, 123 ) }, Error );
+		  });
 	  });
  
   });
